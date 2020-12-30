7754753600f50c98c4e264683ac0734fbbaffa63
Sirrobindoger <robindoger@gmail.com>
2020-12-30 14:20:39 -0500

    pages

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Button from "react-bootstrap/Button";
+import login from "./login"
+
+const Home = () => {
+    return <div>
+        Hello
+        <login />
+    </div>
+}
+
+export default Home;
