45467420ea16c78ac340cb683e8c23a8d49f55f9
Sirrobindoger <robindoger@gmail.com>
2021-01-10 13:29:09 -0500

    Providers, navbar

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,6 +1,6 @@
 import {Component} from "react"
-import { parseCookies, setCookie, destroyCookie } from 'nookies'
-import {Container, Col, Image, Row} from "react-bootstrap"
+import {parseCookies, setCookie, destroyCookie } from 'nookies'
+import {Container, Col, Image, Row, Form, Nav} from "react-bootstrap"
 import {Navigation} from "../../components/navigation"
 
 class Dashboard extends Component {
@@ -12,30 +12,56 @@ class Dashboard extends Component {
 	renderLogos() {
 		const logos = []
 		{this.props.providers.map((val, i) => {
-			logos.push(				
-				<Image
-					style={{height: "70px", width: "150px"}}
-					key={i}
-					src={val.logo}
-					fluid={true}
-					className={"mr-5 " + (i == 0 ? "ml-2" : "" )}
-				/>
+			logos.push(	
+                <img
+                    src={val.logo}
+                    height="50"
+                    width="150"
+                />
 			)
 		})}
 		return logos
 	}
 
 	render() {
-
         return ( 
             <div>
                 <Navigation Fourms={true}/>
 				<Container fluid={true} className="mx-3 mt-3">
-					<p>Providers(s)</p>
-					<Row className="">
+                    <p><small>Providers(s)</small></p>
+					<Row className="ml-0">   
 						{this.renderLogos()}
 					</Row>
 				</Container>
+                
+                <Container fluid={true} className="mx-3 pt-3">
+                    <h6>Affiliate Guard Forums</h6>
+                    <Row>
+                        <Col>
+                            
+                            <Form>
+                                <Form.Group controlID="searchForms">
+                                    <Form.Label><small>Search</small></Form.Label>
+                                    <Form.Control placeholder="Form name" />
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                        <Col xs={8}>
+                            <p>Filter</p>
+                            <Nav defaultActiveKey="all" className="pr-0">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="all">All</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="waivers">Waivers</Nav.Link>
+                                </Nav.Item>
+                                <Nav.Item>
+                                    <Nav.Link eventKey="hiringcoaches">Hiring Coaches</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
 	}
@@ -49,7 +75,7 @@ export async function getServerSideProps(ctx) {
         const json = await res.json()
         return {
             props: {
-				providers: [json, json]
+				providers: [json]
 			}
         }
 
